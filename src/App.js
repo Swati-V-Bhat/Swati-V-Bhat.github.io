56a9de9c83c9c6e9d037f85a6b4f885b01d79704
Swati-V-Bhat <swativinayakbhat.221ds031@nitk.edu.in>
2023-10-19 01:32:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (<div className="App">
+  <header className="App-header">
+  <h1><center>VoicePen</center></h1>
+    
+  </header>
+</div>
+  
+    
+  );
+  }
+
+export default App;
