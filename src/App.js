8ff724feaa250fc9ee96003533369203e9df78ba
Swati-V-Bhat <swativinayakbhat.221ds031@nitk.edu.in>
2023-10-22 19:19:33 +0530

    added audio recording buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
 import './App.css';
+import React from 'react';
 import logo from './logo.svg';
+import MicRecorder from "mic-recorder-to-mp3"
+import { useEffect, useState, useRef } from "react"
+import axios from "axios"
 
 function App() {
+  const recorder = useRef(null) //Recorder
+  const audioPlayer = useRef(null) //Ref for the HTML Audio Tag
+  const [blobURL, setBlobUrl] = useState(null)
+  const [audioFile, setAudioFile] = useState(null)
+  const [isRecording, setIsRecording] = useState(null)
+
+  useEffect(() => {
+    //Declares the recorder object and stores it inside of ref
+    recorder.current = new MicRecorder({ bitRate: 128 })
+  }, [])
+
+  const startRecording = () => {
+    // Check if recording isn't blocked by browser
+    recorder.current.start().then(() => {
+      setIsRecording(true)
+    })
+  }
+
+  const stopRecording = () => {
+    recorder.current
+      .stop()
+      .getMp3()
+      .then(([buffer, blob]) => {
+        const file = new File(buffer, "audio.mp3", {
+          type: blob.type,
+          lastModified: Date.now(),
+        })
+        const newBlobUrl = URL.createObjectURL(blob)
+        setBlobUrl(newBlobUrl)
+        setIsRecording(false)
+        setAudioFile(file)
+      })
+      .catch((e) => console.log(e))
+  }
   return (<div className="App">
+    
   <header className="App-header">
   <h1><center>VoicePen</center></h1>
-    
   </header>
+
+  <body>
+  <audio ref={audioPlayer} src={blobURL} controls='controls' />
+      <div>
+        <button disabled={isRecording} onClick={startRecording}>
+          START
+        </button>
+        <button disabled={!isRecording} onClick={stopRecording}>
+          STOP
+        </button>
+        <button>SUBMIT</button>
+      </div>
+
+</body>
 </div>
   
     
