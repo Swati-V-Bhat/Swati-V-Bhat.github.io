b56423312a6dd0ed2c6384ba5af388e138b29116
Swati-V-Bhat <swativinayakbhat.221ds031@nitk.edu.in>
2023-10-23 09:26:12 +0530

    transcribing recorded audio

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import MicRecorder from "mic-recorder-to-mp3"
 import { useEffect, useState, useRef } from "react"
 import axios from "axios"
 
-import dotenv from 'dotenv';
+
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const YourAPIKey = process.env.REACT_APP_API_KEY;
 
+//Set AssemblyAI Axios Header
 const assembly = axios.create({
   baseURL: "https://api.assemblyai.com/v2",
   headers: {
@@ -27,6 +29,7 @@ function App() {
   const [blobURL, setBlobUrl] = useState(null)
   const [audioFile, setAudioFile] = useState(null)
   const [isRecording, setIsRecording] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     //Declares the recorder object and stores it inside of ref
@@ -56,6 +59,70 @@ function App() {
       })
       .catch((e) => console.log(e))
   }
+ //State Variables
+  const [uploadURL, setUploadURL] = useState("")
+  const [transcriptID, setTranscriptID] = useState("")
+  const [transcriptData, setTranscriptData] = useState("")
+  const [transcript, setTranscript] = useState("")
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  useEffect(() => {
+    if (audioFile) {
+      assembly
+        .post("/upload", audioFile)
+        .then((res) => setUploadURL(res.data.upload_url))
+        .catch((err) => console.error(err))
+    }
+  }, [audioFile])
+
+    // Submit the Upload URL to AssemblyAI and retrieve the Transcript ID
+    const submitTranscriptionHandler = () => {
+      
+      assembly
+        .post("/transcript", {
+          audio_url: uploadURL,
+        })
+        .then((res) => {
+          setTranscriptID(res.data.id)
+          checkStatusHandler()
+        })
+        .catch((err) => console.error(err))
+    }
+    // Check the status of the Transcript and retrieve the Transcript Data
+  const checkStatusHandler = async () => {
+    setIsLoading(true)
+    try {
+      await assembly.get(`/transcript/${transcriptID}`).then((res) => {
+        setTranscriptData(res.data)
+		
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+  
+    // Periodically check the status of the Transcript
+    useEffect(() => {
+      const interval = setInterval(() => {
+        if (transcriptData.status !== "completed" && isLoading) {
+          checkStatusHandler()
+        } else {
+          setIsLoading(false)
+          setTranscript(transcriptData.text)
+  
+          clearInterval(interval)
+        }
+      }, 1000)
+      return () => clearInterval(interval)
+    },)
+    
+
   return (<div className="App">
     
   <header className="App-header">
@@ -65,15 +132,25 @@ function App() {
   <body>
   <audio ref={audioPlayer} src={blobURL} controls='controls' />
       <div>
-        <button disabled={isRecording} onClick={startRecording}>
+       {/*   <button disabled={isRecording} onClick={startRecording}>
+          START
+        </button> */}
+        <input type="file" accept="audio/*" onChange={handleFileChange} />
+
+        <button onClick={startRecording}>
           START
         </button>
         <button disabled={!isRecording} onClick={stopRecording}>
           STOP
         </button>
-        <button>SUBMIT</button>
+        <button onClick={submitTranscriptionHandler}>SUBMIT</button>
+        
       </div>
-
+      {transcriptData.status === "completed" ? (
+        <p>{transcript}</p>
+      ) : (
+        <p>{transcriptData.status}</p>
+      )}
 </body>
 </div>
   
